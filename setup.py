39712ed5ece3068a82ffabd2a9df02b17965260d
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-11-03 15:50:08 -0700

    Update the version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ classes derived from wx.
 
 setup(
     name = "accesspanel",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     install_requires = [],
     description = DESCRIPTION,
